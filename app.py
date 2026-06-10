1364c4d4155d0683d6b02bfb6eecab85182dc9a2
Diya Ajay Antony <24f2007162@ds.study.iitm.ac.in>
2026-06-10 15:51:02 +0530

    Database schema setup of users,treks and bookings

--- app.py
+++ app.py
@@ -1,9 +1,88 @@
-from flask import Flask
+from flask import Flask, render_template
+from extensions import db, login_manager
+from models import User, Trek
+import models
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "trekking-secret-key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///trekking.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(int(user_id))
+
+
 @app.route("/")
 def home():
-  return "Welcome to Trekking Management System"
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
+
+    with app.app_context():
+
+        db.create_all()
+
+        admin = User.query.filter_by(
+            email="admin@musafir.com"
+        ).first()
+
+        if not admin:
+            admin = User(
+                name="Musafir Admin",
+                email="admin@musafir.com",
+                password="musafir123",
+                role="admin"
+            )
+
+            db.session.add(admin)
+            db.session.commit()
+
+            print("Admin created successfully")
+
+        staff = User.query.filter_by(
+            email="staff@musafir.com"
+        ).first()
+
+        if not staff:
+            staff = User(
+                name="Rahul Kumar",
+                email="staff@musafir.com",
+                password="staff123",
+                role="staff"
+            )
+
+            db.session.add(staff)
+            db.session.commit()
+
+            print("Staff created")
+
+        trek = Trek.query.filter_by(
+            name="Rajgad Trek"
+        ).first()
+
+        if not trek:
+            trek = Trek(
+                name="Rajgad Trek",
+                location="Pune",
+                difficulty="Moderate",
+                duration=2,
+                available_slots=25,
+                staff_id=staff.id,
+                status="Open"
+            )
+
+            db.session.add(trek)
+            db.session.commit()
+
+            print("Trek created")
+
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,50 @@
+from extensions import db
+from datetime import datetime,UTC
+from flask_login import UserMixin
+
+class User(UserMixin,db.Model):
+  __tablename__ = "users"
+  id = db.Column(db.Integer, primary_key = True)
+  name = db.Column(db.String(100),nullable =  False)
+  email = db.Column(db.String(120), unique = True,nullable = False)
+  password = db.Column(db.String(255),nullable = False)
+  role = db.Column(db.String(20),nullable = False)
+  status = db.Column(db.String(20),default = "active")
+  staff_profile = db.relationship("StaffProfile",backref = "user",uselist =  False)
+  assigned_treks = db.relationship("Trek",backref = "staff",lazy = True)
+  bookings = db.relationship("Booking",backref = "user",lazy = True)
+
+class Trek(db.Model):
+  __tablename__ = "treks"
+  id = db.Column(db.Integer,primary_key = True)
+  name = db.Column(db.String(120),nullable =  False)
+  location = db.Column(db.String(100),nullable = False)
+  difficulty = db.Column(db.String(20),nullable = False)
+  duration = db.Column(db.Integer,nullable =  False)
+  available_slots = db.Column(db.Integer,nullable = False)
+  staff_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+  status = db.Column(db.String(20),nullable = False)
+  start_date = db.Column(db.Date)
+  end_date = db.Column(db.Date)
+  bookings = db.relationship("Booking",backref = "trek",lazy = True)
+  description = db.Column(db.Text)
+  created_at = db.Column(db.DateTime, default = lambda: datetime.now(UTC))  
+
+class Booking(db.Model):
+  __tablename__ = "bookings"
+  id = db.Column(db.Integer,primary_key = True)
+  user_id = db.Column(db.Integer,db.ForeignKey("users.id"),nullable = False)
+  trek_id = db.Column(db.Integer,db.ForeignKey("treks.id"),nullable = False)
+  booking_date = db.Column(db.DateTime, default = lambda: datetime.now(UTC))
+  status = db.Column(db.String(20),default = "Booked")
+  payment_status = db.Column(db.String(20),default = "Pending")
+
+class StaffProfile(db.Model):
+  __tablename__ = "staff_profiles"
+  id = db.Column(db.Integer,primary_key = True)
+  user_id = db.Column(db.Integer,db.ForeignKey("users.id"),unique = True,nullable = False)
+  contact_number =db.Column(db.String(15))
+  experience_years = db.Column(db.Integer,default = 0)  
+  bio = db.Column(db.Text)
+  approval_status = db.Column(db.String(20),default = "pending")
+
